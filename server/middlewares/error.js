148ca620223789dccfb06bda9eb0d11929f49d60
Aryan Khandelwal <60040654+Ark2307@users.noreply.github.com>
2022-06-08 23:30:40 +0530

    updated error.js

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -15,8 +15,26 @@ module.exports = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
 
+  // mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `JSON web token is invalid , try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  // JWT expires
+  if (err.name === "TokenExpireError") {
+    const message = `JSON web token has expired , try again`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
-    error: err.stack,
+    message: err.message,
   });
 };
